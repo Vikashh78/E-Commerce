7ba6de0bb24622d12bf124009c9b9bffaf7936ca
Vikashh78 <srmavikash10@gmail.com>
2026-01-14 02:01:18 +0530

    Refactor Contact page layout; integrate NewsLetterBox component and enhance structure with additional information

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -12,7 +12,7 @@ const NewsLetterBox = () => {
         <p className='text-gray-400 mt-3'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, facere.
         </p>
-        {/* onSubmit() always expects a callback function, not a function call. */}
+        {/* onSubmit={} always expects a callback function, not a function call. */}
         <form onSubmit={onSubmitHandler} className='w-90 sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
             <input type="email" placeholder='Enter your email' required className='w-full sm:flex-1 outline-none'/>
             <button type='submit' className='bg-black text-white text-xs px-10 py-4 cursor-pointer'>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/NewsLetterBox';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='p-2'>
+
+      <div className='text-2xl border-t pt-10 mb-10'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 gap-10 flex flex-col justify-center md:flex-row '>
+        <img className='w-full md:max-w-120' src={assets.contact_img} alt="" />
+        <div className='gap-6 flex flex-col items-start justify-center'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>54302 C.P <br /> Suite 450, Delhi, India</p>
+          <p className='text-gray-500'>Tel: (+91) 555-777-2233 <br /> Email: admin@forever.com</p>
+          <p className='font-semibold text-xl'>Careers at Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore jobs</button>
+        </div>
+      </div>
+
+      <div className='pt-16'>
+        <NewsLetterBox />
+      </div>
+    </div>
   )
 }
 
