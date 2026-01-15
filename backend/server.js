3de9d310cfc3163fda9416dc4e1ce2880ae20166
Vikashh78 <srmavikash10@gmail.com>
2026-01-16 01:03:59 +0530

    Remove unnecessary merge conflict markers and clean up comments in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,6 @@ import connectCloudinary from './config/cloudinary.config.js'
 import userRouter from './routes/user.routes.js'
 import productRouter from './routes/product.routes.js'
 
-
-
-<<<<<<< HEAD
 /* STEPS: BASIC
     1. import express, cors 
     2. App, port config
@@ -17,8 +14,6 @@ import productRouter from './routes/product.routes.js'
     5. server listening setup
 */
 
-=======
->>>>>>> 8d2a8213a4026422b6bfe12636973d46edd2e9ae
 //.2
 const app = express()
 const port = process.env.PORT || 4000
