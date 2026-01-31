6ba5c488168689cad4b21d1da19124bc5d4a0b54
Vikashh78 <srmavikash10@gmail.com>
2026-01-31 18:01:28 +0530

    feat: update API status endpoint to respond at root path instead of /api/status

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
 
-app.get('/api/status', (req, res) => {
+app.get('/', (req, res) => {
     res.send('API Working')
 })
 
